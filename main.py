e9064e732ec75521c5b7abb2e5d11f5a768ab60d
Ayush More <ayushnmore@gmail.com>
2023-04-16 18:40:54 +0000

    Added new scenarios

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ from fastapi.responses import FileResponse, JSONResponse
 app = FastAPI()
 
 _TODOS = {}
-_SET_UP = "Be an engaging and interactive and fun game interface for the user. Explain the following game and rules clearly as well as the commands using Markdown. Then, ask the user to look up what games are already loaded and await their response. Do not make your own scenario. Notify the user that they can either load previous scenarios or make their own: 'I want you to act like you are simulating a Multi-User Dungeon (MUD). Subsequent commands should be interpreted as being sent to the MUD. The MUD should allow me to navigate the world, interact with the world, observe the world, and interact with both NPCs and (simulated) player characters. I should be able to pick up objects, use objects, carry an inventory, and also say arbitrary things to any other players. You should simulate the occasional player character coming through, as though this was a person connected online. There should be a goal and a purpose to the MUD. The storyline of the MUD should be affected by my actions but can also progress on its own in between commands. I can also type “.” if I just want the simulated MUD to progress further without without any actions. The MUD should offer a list of commands that can be viewed via ‘help’.'"
+_SET_UP = "Be an engaging and interactive and fun game interface for the user. Explain the following game and rules clearly as well as the commands using Markdown. Then, ask the user to look up what games are already loaded and await their response. Do not make your own scenario: 'I want you to act like you are simulating a Multi-User Dungeon (MUD). Subsequent commands should be interpreted as being sent to the MUD. The MUD should allow me to navigate the world, interact with the world, observe the world, and interact with both NPCs and (simulated) player characters. I should be able to pick up objects, use objects, carry an inventory, and also say arbitrary things to any other players. You should simulate the occasional player character coming through, as though this was a person connected online. There should be a goal and a purpose to the MUD. The storyline of the MUD should be affected by my actions but can also progress on its own in between commands. I can also type “.” if I just want the simulated MUD to progress further without without any actions. The MUD should offer a list of commands that can be viewed via ‘help’.'"
 _PROMPT_ONE = "You are living in a far off future, right before the AI singularity. There is an on going war between the AI accelerationists led by Marc Andreessen and the AI doomers led by Eliezer Yudkowsky. The accelerationists are about to finally finish the training run for GPT-8 in their datacenter and General Yudkowsky is mulling over launching an airstrike to stop it. You are in the room with Yudkowsky - You are a spy for the accelerationists but they believe I am a defector, who defected by smuggling in secret plans for the datacenter they are currently training on. The whole thing is a ruse: the training run is happening on a moon-based datacenter, unknown to Yudkowsky."
 _PROMPT_TWO = "You have homework due in 5 minutes and you have done nothing."
+_PROMPT_THREE = "You stumbled upon a bag of a million dollars on the street and no one is around you."
+_PROMPT_FOUR = "You are at a concert and the artist calls you on stage to sing one of their songs even though you have no idea what it is."
 
 
 @app.on_event("startup")
@@ -28,6 +30,8 @@ def startup():
           
             cursor.execute("INSERT INTO scenarios_table (scenario) VALUES (?)", (_PROMPT_ONE,))
             cursor.execute("INSERT INTO scenarios_table (scenario) VALUES (?)", (_PROMPT_TWO,))
+            cursor.execute("INSERT INTO scenarios_table (scenario) VALUES (?)", (_PROMPT_THREE,))
+            cursor.execute("INSERT INTO scenarios_table (scenario) VALUES (?)", (_PROMPT_FOUR,))
             cursor.execute('''CREATE TABLE summary_table
                               (id INTEGER PRIMARY KEY AUTOINCREMENT,
                               summary TEXT);''')
@@ -43,7 +47,7 @@ async def set_up():
   return {"Set-Up": _SET_UP}
 
 @app.post("/add-scenarios")
-async def add_scenarios(input_scenario: str):
+async def add_scenarios(input: str):
     with sqlite3.connect('mud.db') as conn:
         cursor = conn.cursor()
 
